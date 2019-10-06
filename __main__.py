c4f46e45bb32ab508688b22d57619494a3c55a4c
bigmaxattack37 <bigmaxmarcus@gmail.com>
2019-10-06 18:43:07 -0500

    added a way to kill and a way to get killed

--- __main__.py
+++ __main__.py
@@ -27,10 +27,24 @@ class Game:
 
         self.mouse_x = 0
         self.mouse_y = 0
+        self.mouse_press = False
 
         self.enemies = []
         for i in range(20):
-            self.enemies.append(Enemy(random.randint(0,root.winfo_screenwidth()),random.randint(0,root.winfo_screenheight())))
+            side = random.randint(1,4)
+            if side == 1:
+                x = random.randint(0,root.winfo_screenwidth())
+                y = 0
+            elif side == 2:
+                x = root.winfo_screenwidth()
+                y = random.randint(0,root.winfo_screenheight())
+            elif side == 3:
+                x = random.randint(0,root.winfo_screenwidth())
+                y = root.winfo_screenheight()
+            else:
+                x = 0
+                y = random.randint(0,root.winfo_screenheight())
+            self.enemies.append(Enemy(x,y,50))
 
 
     def key_press(self,event):
@@ -43,6 +57,12 @@ class Game:
     def motion(self,event):
         self.mouse_x = event.x
         self.mouse_y = event.y
+
+    def click(self,event):
+        self.mouse_press = True
+
+    def un_click(self,event):
+        self.mouse_press = False
 
     def update(self):
         self.delta_time = time.time()-self.last_time
@@ -80,23 +100,26 @@ class Player:
         self.vel_x = 0
         self.vel_y = 0
 
+        self.dead = False
+
         self.width = root.winfo_screenwidth()/43
         self.height = root.winfo_screenwidth()/60
 
         self.photo = PhotoImage(file='Astronaut.gif')
 
-        if 45/self.width > 1:
-            self.photo = self.photo.zoom(int(45/self.width),1)
+        if self.width/45 > 1:
+            self.photo = self.photo.zoom(int(self.width/45),1)
         else:
-            self.photo = self.photo.subsample(int(self.width/45),1)
-        if 64/self.width > 1:
-            self.photo = self.photo.zoom(1,int(64/self.width))
+            self.photo = self.photo.subsample(int(45/self.width),1)
+        if self.width/64 > 1:
+            self.photo = self.photo.zoom(1,int(self.width/64))
         else:
-            self.photo = self.photo.subsample(1,int(self.width/64))
+            self.photo = self.photo.subsample(1,int(64/self.width))
 
     def update(self):
-        self.x += self.vel_x*game.delta_time
-        self.y += self.vel_y*game.delta_time
+        if self.dead == False:
+            self.x += self.vel_x*game.delta_time
+            self.y += self.vel_y*game.delta_time
 
         self.render()
 
@@ -107,42 +130,57 @@ class Player:
         x = game.mouse_x
         y = game.mouse_y
 
-        canvas.create_line(self.x,self.y,x,y,width=5,fill='white')
+        if game.mouse_press:
+            fill = 'green'
+        else:
+            fill = 'white'
+
+        canvas.create_line(self.x,self.y,x,y,width=5,fill=fill)
 
 class Enemy:
-    def __init__(self,x,y):
-        self.x = x#root.winfo_screenwidth()/2 - 100
-        self.y = y#root.winfo_screenheight()/2 - 10
+    def __init__(self,x,y,speed):
+        self.x = x
+        self.y = y
 
-        self.width = root.winfo_screenwidth()/43
-        self.height = root.winfo_screenwidth()/60
+        self.speed = speed
+
+        self.width = root.winfo_screenwidth()/50
+        self.height = root.winfo_screenwidth()/30
 
         self.photo = PhotoImage(file='Alien.gif')
 
-        if 45/self.width > 1:
-            self.photo = self.photo.zoom(int(45/self.width),1)
+        if self.width/19 > 1:
+            self.photo = self.photo.zoom(int(self.width/19),1)
         else:
-            self.photo = self.photo.subsample(int(self.width/45),1)
-        if 64/self.width > 1:
-            self.photo = self.photo.zoom(1,int(64/self.width))
+            self.photo = self.photo.subsample(int(19/self.width),1)
+        if self.width/33 > 1:
+            self.photo = self.photo.zoom(1,int(self.width/33))
         else:
-            self.photo = self.photo.subsample(1,int(self.width/64))
+            self.photo = self.photo.subsample(1,int(33/self.width))
 
     def update(self):
         dist_x = game.player.x - self.x
         dist_y = game.player.y - self.y
 
-        a = math.atan2((dist_y),(dist_x))
+        if abs(dist_x) > game.player.width/2+self.width/2 or abs(dist_y) > game.player.height/2+self.height/2:
+            if game.mouse_press:
+                if game.mouse_x > self.x - self.width/2 and game.mouse_x < self.x + self.width/2 and game.mouse_y > self.y - self.height/2 and game.mouse_y < self.y + self.height/2:
+                    game.enemies.remove(self)
+                    game.mouse_press = False
 
-        x = math.cos(a)
-        y = math.sin(a)
+            a = math.atan2((dist_y),(dist_x))
 
-        dist_x = x*100
-        dist_y = y*100
+            x = math.cos(a)
+            y = math.sin(a)
+
+            dist_x = x*self.speed
+            dist_y = y*self.speed
 
 
-        self.x += dist_x*game.delta_time
-        self.y += dist_y*game.delta_time
+            self.x += dist_x*game.delta_time
+            self.y += dist_y*game.delta_time
+        else:
+            game.player.dead = True
 
         self.render()
 
@@ -156,6 +194,8 @@ root.bind('<KeyPress>',game.key_press)
 root.bind('<KeyRelease>',game.key_release)
 
 root.bind('<Motion>',game.motion)
+root.bind('<Button-1>',game.click)
+root.bind('<ButtonRelease-1>',game.un_click)
 
 while game.game == True:
     try:
