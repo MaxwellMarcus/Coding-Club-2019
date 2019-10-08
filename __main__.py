8fb0227b1b5cf81603467bba265ff8883dec5d5d
bigmaxattack37 <bigmaxmarcus@gmail.com>
2019-10-07 21:29:43 -0500

    progress bar

--- __main__.py
+++ __main__.py
@@ -29,23 +29,7 @@ class Game:
         self.mouse_y = 0
         self.mouse_press = False
 
-        self.enemies = []
-        for i in range(20):
-            side = random.randint(1,4)
-            if side == 1:
-                x = random.randint(0,root.winfo_screenwidth())
-                y = 0
-            elif side == 2:
-                x = root.winfo_screenwidth()
-                y = random.randint(0,root.winfo_screenheight())
-            elif side == 3:
-                x = random.randint(0,root.winfo_screenwidth())
-                y = root.winfo_screenheight()
-            else:
-                x = 0
-                y = random.randint(0,root.winfo_screenheight())
-            self.enemies.append(Enemy(x,y,50))
-
+        self.wave = Wave(40)
 
     def key_press(self,event):
         self.keys.append(event.keysym)
@@ -83,30 +67,27 @@ class Game:
             self.player.vel_x = 150
 
         #canvas.create_text(100,100,text='FPS: '+str(int(1/self.delta_time)),fill='white')
-
+        if self.wave:
+            self.wave.update()
+            x = (root.winfo_screenwidth()/4)+(root.winfo_screenwidth()/2*(len(self.wave.enemies)/self.wave.size))
+            self.progress_bar(root.winfo_screenwidth()/4,root.winfo_screenheight()/30*2,x,root.winfo_screenwidth()/4*3,root.winfo_screenheight()/60)
+            canvas.create_text(root.winfo_screenwidth()/2,root.winfo_screenheight()/30,anchor=CENTER,text='Wave',fill='white',font=('TkTextFont',25))
 
         self.player.update()
 
-        for i in self.enemies:
-            i.update()
-
-        if (time.time()-self.time)%2 == 0:
-            side = random.randint(1,4)
-            if side == 1:
-                x = random.randint(0,root.winfo_screenwidth())
-                y = 0
-            elif side == 2:
-                x = root.winfo_screenwidth()
-                y = random.randint(0,root.winfo_screenheight())
-            elif side == 3:
-                x = random.randint(0,root.winfo_screenwidth())
-                y = root.winfo_screenheight()
-            else:
-                x = 0
-                y = random.randint(0,root.winfo_screenheight())
-            self.enemies.append(Enemy(x,y,50))
-
         root.update()
+
+    def progress_bar(self,x,y,x2,x3,height):
+        height = int(height)
+        if not height%2 == 0:
+            height += 1
+        canvas.create_oval(x-height-1,y-height-1,x+height-1,y+height-1,fill='white')
+        canvas.create_oval(x3-height-1,y-height-1,x3+height-1,y+height-1,fill='white')
+        canvas.create_line(x,y,x3,y,fill='white',width=height*2)
+        if not x == x2:
+            canvas.create_oval(x-(height/2-1)-1,y-(height/2-1)-1,x+(height/2-1)-1,y+(height/2-1)-1,fill='red',outline='red')
+            canvas.create_oval(x2-(height/2-1)-1,y-(height/2-1)-1,x2+(height/2-1)-1,y+(height/2-1)-1,fill='red',outline='red')
+        canvas.create_line(x,y,x2,y,fill='red',width=height)
 
 class Player:
     def __init__(self):
@@ -123,6 +104,9 @@ class Player:
 
         self.photo = PhotoImage(file='Astronaut.gif')
 
+        self.laser = False
+        self.laser_time = 0
+
         if self.width/45 > 1:
             self.photo = self.photo.zoom(int(self.width/45),1)
         else:
@@ -137,6 +121,14 @@ class Player:
             self.x += self.vel_x*game.delta_time
             self.y += self.vel_y*game.delta_time
 
+        if game.mouse_press and not self.laser:
+            self.laser = True
+            self.laser_time = time.time()
+
+        if self.laser:
+            if time.time()-self.laser_time > .5:
+                self.laser = False
+
         self.render()
 
     def render(self):
@@ -146,7 +138,7 @@ class Player:
         x = game.mouse_x
         y = game.mouse_y
 
-        if game.mouse_press:
+        if self.laser:
             fill = 'green'
         else:
             fill = 'white'
@@ -154,7 +146,7 @@ class Player:
         canvas.create_line(self.x,self.y,x,y,width=5,fill=fill)
 
 class Enemy:
-    def __init__(self,x,y,speed):
+    def __init__(self,x,y,speed,wave):
         self.x = x
         self.y = y
 
@@ -174,15 +166,17 @@ class Enemy:
         else:
             self.photo = self.photo.subsample(1,int(33/self.width))
 
+        self.wave = wave
+
     def update(self):
         dist_x = game.player.x - self.x
         dist_y = game.player.y - self.y
 
         if abs(dist_x) > game.player.width/2+self.width/2 or abs(dist_y) > game.player.height/2+self.height/2:
-            if game.mouse_press:
+            if game.player.laser:
                 if game.mouse_x > self.x - self.width/2 and game.mouse_x < self.x + self.width/2 and game.mouse_y > self.y - self.height/2 and game.mouse_y < self.y + self.height/2:
-                    game.enemies.remove(self)
-                    game.mouse_press = False
+                    game.player.laser = False
+                    self.wave.enemies.remove(self)
 
             a = math.atan2((dist_y),(dist_x))
 
@@ -203,6 +197,31 @@ class Enemy:
     def render(self):
         canvas.create_image(self.x,self.y,anchor=CENTER,image=self.photo)
         #canvas.create_rectangle(self.x+self.width/2,self.y+self.height/2,self.x-self.width/2,self.y-self.height/2,fill='red',outline='red')
+
+class Wave:
+    def __init__(self,size):
+        self.size = size
+
+        self.enemies = []
+        for i in range(size):
+            side = random.randint(1,4)
+            if side == 1:
+                x = random.randint(0,root.winfo_screenwidth())
+                y = 0
+            elif side == 2:
+                x = root.winfo_screenwidth()
+                y = random.randint(0,root.winfo_screenheight())
+            elif side == 3:
+                x = random.randint(0,root.winfo_screenwidth())
+                y = root.winfo_screenheight()
+            else:
+                x = 0
+                y = random.randint(0,root.winfo_screenheight())
+            self.enemies.append(Enemy(x,y,50,self))
+
+    def update(self):
+        for i in self.enemies:
+            i.update()
 
 game = Game()
 
